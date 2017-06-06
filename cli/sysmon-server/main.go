ceceefef18e4f0d6edfabc684f62558d35118075
Alberto Cortés <alcortesm@gmail.com>
2017-06-06 10:55:38 +0200

    improve readability

--- cli/sysmon-server/main.go
+++ cli/sysmon-server/main.go
@@ -12,10 +12,14 @@ import (
 func main() {
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
-	s := server.New(20, time.Second)
+
+	nSamples := 20
+	samplingPeriod := time.Second
+	s := server.New(nSamples, samplingPeriod)
 	if err := s.Connect(); err != nil {
 		log.Fatal(err)
 	}
+
 	<-c // block until os.Interrupt is receieved
 	if err := s.Disconnect(); err != nil {
 		log.Fatal(err)
